3c25df846bb7060de4e056e1c5e427d4441fedce
MomodouJallow <galleh.mj@gmail.com>
2024-04-20 02:52:13 +0300

    functionality to support creating, reading, and cancelling of orders

---
+++ src/main/java/com/example/JallowBooks/bookOrders/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.example.JallowBooks.bookOrders;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<TheOrder, Long> {
+
+    @Query("SELECT o.id, o.userId, o.status, o.createdAt, o.updatedAt FROM TheOrder o WHERE o.userId = :userId")
+    List<OrderResponse> findByUserId(Long userId);
+}
